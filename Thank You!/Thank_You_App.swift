ad7c50a4502d639eb455b11b79fa49aee5758c9f
505y2 <91029055+505y2@users.noreply.github.com>
2024-09-17 14:02:35 -0400

    Initial Commit

---
+++ Thank You!/Thank_You_App.swift
@@ -0,0 +1,17 @@
+//
+//  Thank_You_App.swift
+//  Thank You!
+//
+//  Created by Tyler Forstrom on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Thank_You_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
